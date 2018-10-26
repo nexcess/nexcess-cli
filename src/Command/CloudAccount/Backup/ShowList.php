bd519e0a474f906961c3d8cd7463d3e6b258e676
Cal Evans <cevans@nexcess.net>
2018-10-26 07:58:10 -0400

    Cosmetic

--- src/Command/CloudAccount/Backup/ShowList.php
+++ src/Command/CloudAccount/Backup/ShowList.php
@@ -46,7 +46,7 @@ class ShowList extends ShowListCommand {
   const ARGS = [];
 
   /** {@inheritDoc} */
-  public function execute(Input $input,  Output $output) {
+  public function execute(Input $input, Output $output) {
     $cloud_id = Util::filter(
       $input->getOption('cloud_account_id'),
       Util::FILTER_INT
@@ -64,15 +64,15 @@ class ShowList extends ShowListCommand {
 
   /** {@inheritDoc} */
   protected function _getSummary(array $details) : array {
-    $details =  array_map(
-      function($backup_array) {
+    $details = array_map(
+      function ($backup_array) {
         $timestamp = Util::filter(
           $backup_array['filedate'], Util::FILTER_INT
         );
         $new_file_date = new \DateTimeImmutable();
         $new_file_date = $new_file_date->setTimestamp($timestamp);
         $backup_array['filedate'] = $new_file_date->format('Y-m-d h:i:s');
-        $backup_array['complete'] = ($backup_array['filedate']?'YES':'NO');
+        $backup_array['complete'] = ($backup_array['filedate'] ? 'YES' : 'NO');
         return $backup_array;
       },
       $details
