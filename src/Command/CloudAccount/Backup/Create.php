b45b7b39789d684415916734af408e66fa5757b2
Cal Evans <cevans@nexcess.net>
2018-11-01 08:29:17 -0400

    WORKS

---
+++ src/Command/CloudAccount/Backup/Create.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * @package Nexcess-CLI
+ * @license https://opensource.org/licenses/MIT
+ * @copyright 2018 Nexcess.net, LLC
+ */
+
+declare(strict_types = 1);
+
+namespace Nexcess\Sdk\Cli\Command\CloudAccount\Backup;
+
+use Nexcess\Sdk\ {
+  Resource\CloudAccount\Endpoint,
+  Util\Config,
+  Util\Util,
+  Cli\Console
+};
+
+use Nexcess\Sdk\Cli\Command\Create as CreateCommand;
+use Symfony\Component\Console\ {
+  Input\InputArgument as Arg,
+  Input\InputInterface as Input,
+  Input\InputOption as Opt,
+  Output\OutputInterface as Output
+};
+
+/**
+ * Creates a new Cloud Account.
+ */
+class Create extends CreateCommand {
+
+  /** {@inheritDoc} */
+  const ARGS = [];
+
+  /** {@inheritDoc} */
+  const ENDPOINT = Endpoint::class;
+
+  /** {@inheritDoc} */
+  const SUMMARY_KEYS = ['filename','complete'];
+
+  /** {@inheritDoc} */
+  const INPUTS = [];
+
+  /** {@inheritDoc} */
+  const NAME = 'cloud-account:backup:create';
+
+  /** {@inheritDoc} */
+  const OPTS = [
+    'cloud_account_id|c' => [OPT::VALUE_REQUIRED],
+    'download|d' => [OPT::VALUE_REQUIRED],
+];
+
+  /** {@inheritDoc} */
+  const RESTRICT_TO = [Config::COMPANY_NEXCESS];
+
+  /**
+   * {@inheritDoc}
+   */
+  public function execute(Input $input, Output $output) {
+    $this->getApplication()->getConfig()->set(
+      'wait',
+      ['timeout'=>9999,'interval'=>5]
+    );
+
+    $app = $this->getApplication();
+    $endpoint = $this->_getEndpoint();
+
+    $cloud_id = Util::filter(
+      $input->getOption('cloud_account_id'),
+      Util::FILTER_INT
+    );
+
+    $app->say($this->getPhrase('creating'));
+    $model = $this->_getEndpoint()->retrieve($cloud_id);
+
+    $downloadClosure = null;
+
+    if ($input->getOption('download')) {
+      $downloadClosure = $this->_downloadClosure();
+    }
+
+    $backup = $endpoint->createBackup($model)
+      ->waitUntil($this->_waitClosure())
+      ->then($downloadClosure)
+      ->wait();
+
+    $this->_saySummary($backup->toArray(), $input->getOption('json'));
+
+    $app->say(
+      $this->getPhrase('done')
+    );
+    return Console::EXIT_SUCCESS;
+  }
+
+  /**
+   * Used in the wait() of a Guzzle promise. Checks to see if the download is
+   * complete.
+   *
+   * @return bool
+   *
+   */
+  protected function _waitClosure() : callable {
+    $cloud_id = Util::filter(
+      $this->getApplication()->getIO()[Console::GET_IO_INPUT]
+        ->getOption('cloud_account_id'),
+      Util::FILTER_INT
+    );
+    return function ($backup) use ($cloud_id) {
+      return (
+        $backup->getEndPoint()
+          ->getBackup(
+            $backup->getEndPoint()->retrieve($cloud_id),
+            $backup->get('filename')
+          )->wait()
+        )->get('complete');
+    };
+  }
+
+  /**
+   * Returns a closure that when used in the then() of a Guzzle promise, will
+   * download the backup just created.
+   *
+   * @return callable
+   */
+  protected function _downloadClosure() : callable {
+    $cloud_id = Util::filter(
+      $this->getApplication()->getIO()[Console::GET_IO_INPUT]
+        ->getOption('cloud_account_id'),
+      Util::FILTER_INT
+    );
+    return function ($backup) use ($cloud_id) {
+        $backup = $backup->getEndPoint()
+                    ->getBackup(
+                      $backup->getEndPoint()->retrieve($cloud_id),
+                      $backup->get('filename')
+                    )->wait();
+        $backup->getEndPoint()->downloadBackup(
+          $backup->getEndPoint()->retrieve($cloud_id),
+          $backup->get('filename'),
+          $this->getApplication()->getIO()[Console::GET_IO_INPUT]
+            ->getOption('download')
+        );
+      return $backup;
+    };
+  }
+}
