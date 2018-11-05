f62d1e2d51890dcf71635d9cbcaabc0586e65a38
Cal Evans <cevans@nexcess.net>
2018-11-05 14:32:43 -0500

    First attempt

---
+++ src/Command/CloudAccount/Backup/Delete.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @package Nexcess-CLI
+ * @license https://opensource.org/licenses/MIT
+ * @copyright 2018 Nexcess.net, LLC
+ */
+
+declare(strict_types = 1);
+
+namespace Nexcess\Sdk\Cli\Command\CloudAccount\Backup;
+
+use Closure;
+
+use Nexcess\Sdk\ {
+  Resource\CloudAccount\Backup,
+  Resource\CloudAccount\Endpoint,
+  Resource\Promise,
+  Util\Config,
+  Util\Util
+};
+
+use Nexcess\Sdk\Cli\ {
+  Command\CloudAccount\CloudAccountException,
+  Command\Create as CreateCommand,
+  Console
+};
+
+use Symfony\Component\Console\ {
+  Input\InputArgument as Arg,
+  Input\InputInterface as Input,
+  Input\InputOption as Opt,
+  Output\OutputInterface as Output
+};
+
+/**
+ * Creates a new Cloud Account.
+ */
+class Delete extends CreateCommand {
+
+  /** {@inheritDoc} */
+  const ARGS = [];
+
+  /** {@inheritDoc} */
+  const ENDPOINT = Endpoint::class;
+
+  /** {@inheritDoc} */
+  const SUMMARY_KEYS = ['filename'];
+
+  /** {@inheritDoc} */
+  const INPUTS = [];
+
+  /** {@inheritDoc} */
+  const NAME = 'cloud-account:backup:delete';
+
+  /** {@inheritDoc} */
+  const OPTS = [
+    'cloud_account_id|c' => [OPT::VALUE_REQUIRED],
+    'file_name|f' => [OPT::VALUE_REQUIRED]
+  ];
+
+  /** {@inheritDoc} */
+  const RESTRICT_TO = [Config::COMPANY_NEXCESS];
+
+  /**
+   * {@inheritDoc}
+   */
+  public function execute(Input $input, Output $output) {
+    $app = $this->getApplication();
+    $cloud_account_id = Util::filter(
+      $input->getOption('cloud_account_id'),
+      Util::FILTER_INT
+    );
+    $file_name = $input->getOption('file_name');
+
+    $endpoint = $this->_getEndpoint();
+    $cloud = $endpoint->retrieve($cloud_account_id);
+    $backup = $endpoint->getBackup($cloud,$file_name);
+
+    $app->say($this->getPhrase('Deleting',['file_name' => $file_name]));
+    $backup->delete($cloud);
+    $app->say($this->getPhrase('done'));
+
+    return Console::EXIT_SUCCESS;
+  }
+
+}
