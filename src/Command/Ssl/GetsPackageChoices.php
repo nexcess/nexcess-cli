e9350fd424643e0e929f09cc0c4a790cfbc50922
Cal Evans <cevans@nexcess.net>
2018-12-06 12:15:13 -0500

    Add ssl-cert support to the cli

---
+++ src/Command/Ssl/GetsPackageChoices.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package Nexcess-CLI
+ * @license https://opensource.org/licenses/MIT
+ * @copyright 2018 Nexcess.net, LLC
+ */
+
+declare(strict_types = 1);
+
+namespace Nexcess\Sdk\Cli\Command\Ssl;
+
+use Nexcess\Sdk\Cli\ {
+  Command\ChoiceException,
+  Console
+};
+
+use Nexcess\Sdk\ {
+  Resource\Package\Endpoint as Package,
+  Resource\Readable
+};
+
+trait GetsPackageChoices {
+
+  /**
+   * {@inheritDoc} Command\Command::getConsole()
+   */
+  abstract public function getConsole() : Console;
+
+  /**
+   * {@inheritDoc} Command\InputCommand::getInput()
+   */
+  abstract public function getInput(
+    string $name = null,
+    bool $optional = true
+  );
+
+  /**
+   * {@inheritDoc} Command\Command::_getEndpoint()
+   */
+  abstract protected function _getEndpoint(string $endpoint = null) : Readable;
+
+  /**
+   * {@inheritDoc} Command\InputCommand::_padColumns()
+   */
+  abstract protected function _padColumns(
+    array $details,
+    array $columns = null
+  ) : array;
+
+  /** @var array {@inheritDoc} InputCommand::$_choices */
+  protected $_choices = [];
+
+  /**
+   * Gets a map of available cloud accounts.
+   *
+   * @param bool $format Apply formatting?
+   * @return string[] Map of id:description pairs
+    FIX
+   */
+  protected function _getPackageChoices(bool $format = true) : array {
+    if (empty($this->_choices['package'])) {
+      $this->_choices['package'] = array_column(
+        $this->_getEndpoint(Package::class)
+          ->list([
+            'type' => 'virt-guest-cloud',
+            'environment_type' => 'production'
+          ])
+          ->toArray(true),
+        null,
+        'id'
+      );
+      if (empty($this->_choices['package'])) {
+        throw new ChoiceException(
+          ChoiceException::NO_CLOUD_ACCOUNT_PACKAGE_CHOICES
+        );
+      }
+    }
+
+    $choices = $this->_choices['package'];
+
+    if ($format) {
+      $choices = $this->_padColumns($choices, ['name', 'monthly_fee']);
+      $console = $this->getConsole();
+      foreach ($choices as $id => $package) {
+        $choices[$id] = $console->translate(
+          'console.cloud_account.choices.package',
+          $package
+        );
+      }
+      return $choices;
+    }
+
+    return array_column($choices, 'name', 'id');
+  }
+}
---
+++ src/Command/Ssl/Show.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @package Nexcess-CLI
+ * @license https://opensource.org/licenses/MIT
+ * @copyright 2018 Nexcess.net, LLC
+ */
+
+declare(strict_types = 1);
+
+namespace Nexcess\Sdk\Cli\Command\Ssl;
+
+use DateTimeImmutable as DateTime;
+
+use Nexcess\Sdk\ {
+  Resource\Ssal\Endpoint,
+  Util\Config
+};
+
+use Nexcess\Sdk\Cli\ {
+  Command\Show as ShowCommand
+};
+
+use Symfony\Component\Console\ {
+  Input\InputInterface as Input,
+  Input\InputOption as Opt,
+  Output\OutputInterface as Output
+};
+
+/**
+ * Creates a new Cloud Account.
+ */
+class Show extends ShowCommand {
+  use GetsCloudAccountChoices;
+
+  /** {@inheritDoc} */
+  const ENDPOINT = Endpoint::class;
+
+  /** {@inheritDoc} */
+  const NAME = 'ssl:show';
+
+  /** {@inheritDoc} */
+  const OPTS = [
+    'domain' => [OPT::VALUE_REQUIRED],
+    'ip' => [Opt::VALUE_REQUIRED]
+  ];
+
+  /** {@inheritDoc} */
+  const RESTRICT_TO = [Config::COMPANY_NEXCESS];
+
+  /** {@inheritDoc} */
+  const SUMMARY_KEYS = [
+    'id',
+    'app',
+    'deploy_date',
+    'domain',
+    'environment',
+    'ip',
+    'location',
+    'service',
+    'service.status',
+    'status',
+    'temp_domain',
+    'unix_username'
+  ]; // fix
+
+  /**
+   * {@inheritDoc}
+   */
+  public function initialize(Input $input, Output $output) {
+    parent::initialize($input, $output);
+
+    if ($this->_input['id'] === null) {
+      $lookup = $input->getOption('domain') ?? $input->getOption('ip');
+      if (! empty($lookup)) {
+        $this->_lookupChoice('id', $lookup);
+      }
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function _getChoices(string $name, bool $format = true) : array {
+    switch ($name) {
+      case 'id':
+        return $this->_getSslChoices($format);
+      default:
+        return parent::_getChoices($name, $format);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   FIX
+   */
+  protected function _getSummary(array $details) : array {
+    $details = parent::_getSummary($details);
+
+    $details['app'] = $this->getPhrase('summary_app', $details['app']);
+    $details['deploy_date'] = (new DateTime("@{$details['deploy_date']}"))
+      ->format('Y-m-d H:i:s T');
+    $details['location'] = $this->getPhrase(
+      'summary_location',
+      $details['location']
+    );
+    $details['service'] = $this->getPhrase(
+      'summary_service',
+      $details['service']
+    );
+
+    return $details;
+  }
+}
