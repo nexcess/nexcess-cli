4a3fc3f11028d425937e877f751757e30d628baf
Cal Evans <cevans@nexcess.net>
2018-12-12 15:37:21 -0500

    Cosmetic

--- src/Command/Ssl/ShowList.php
+++ src/Command/Ssl/ShowList.php
@@ -13,7 +13,7 @@ use Nexcess\Sdk\Resource\Ssl\Endpoint;
 use Nexcess\Sdk\Cli\Command\ShowList as ShowListCommand;
 
 /**
- * Lists Ssl.
+ * Lists Ssl Certificates
  */
 class ShowList extends ShowListCommand {
 
@@ -24,5 +24,5 @@ class ShowList extends ShowListCommand {
   const NAME = 'ssl:list';
 
   /** {@inheritDoc} */
-  const SUMMARY_KEYS = ['id', 'common_name']; // fix
+  const SUMMARY_KEYS = ['id', 'common_name'];
 }
